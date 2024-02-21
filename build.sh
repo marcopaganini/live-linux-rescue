5e37dd82fede10273240e607cd167fa150d19721
Marco Paganini <paganini@paganini.net>
2024-02-20 23:51:55 -0800

    Multiple fixes.

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -eu -o pipefail
 
+sudo lb clean
 lb config -d bookworm --debian-installer-distribution bookworm --archive-areas "main contrib non-free-firmware"
-cp pkgs.list.chroot ./config/package-lists
-lb build
+sudo cp pkgs.list.chroot ./config/package-lists
+sudo lb build
 
 echo "To rebuild, just run \"lb clean; lb config; lb build\""
